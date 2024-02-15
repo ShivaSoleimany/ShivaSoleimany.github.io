5738f81e0f88476e463eadfc70901e234bead512
ShivaSoleimany <shiva.soleimany.dzch@gmail.com>
2024-02-15 11:29:16 -0500

    process and send back

--- scripts/genre.py
+++ scripts/genre.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/path/to/your/python/script.py', methods=['POST'])
 def process_selected_genres():
-    selected_genres = request.form.get('selectedGenres').split(",")
-    # Do whatever you need to do with the selected genres
-    return "Selected genres: " + ", ".join(selected_genres)
+    data = request.get_json()
+    selected_genres = data.get('selectedGenres', [])
+    uppercase_genres = [genre.upper() for genre in selected_genres]
+    return jsonify(uppercase_genres)
 
 if __name__ == '__main__':
     app.run(debug=True)
